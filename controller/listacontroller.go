4ede9232feca44c225b19a6a87b1d5c4ced79db6
SantsGu <gustavosht13@hotmail.com>
2024-02-08 19:20:34 -0300

    subindo alterações do controller

--- controller/listacontroller.go
+++ controller/listacontroller.go
@@ -1,23 +1,52 @@
 package controller
 
-import "github.com/SantsGu/lista-de-tarefa/repository"
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
 
-func GetAll() {
+	"github.com/SantsGu/lista-de-tarefa/service"
+)
+
+
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	lista := service.FindAll()
+	jsonResp, err := json.Marshal(lista)
+	if err != nil{
+		http.Error(w, "Erro ao conveter para json", http.StatusInternalServerError)
+		return
+	}
+
+	response(w, http.StatusOK, jsonResp)
+}
+
+func GetId(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetID() {
+func Update(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Update() {
+func Create(w http.ResponseWriter, r *http.Request) {
+	 path := r.URL.Path
+	 pieces := strings.Split(path, "/")
+	 if len(pieces) <3{
+		http.NotFound(w, r)
+		return 
+	 }
+	 service.SaveTask(pieces[2])
+	 response(w, http.StatusOK, nil)
 
 }
 
-func Create() {
+func Delete(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Delete() {
-
+func response(w http.ResponseWriter, statusCode int, json []byte){
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(statusCode)
+	w.Write(json)
 }
